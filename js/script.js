0286f1b1af9e555f5a5162d621b656cb6553d475
Vinicius Braga <contato@viniciusbraga.com>
2012-09-26 17:00:04 -0300

    Add and apply the basic JavaScript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    // Chama um evento do Analytics em caso de clique em links externos.
+    $("a[rel*='external']").click(function(){
+      trackEvent('Destino externo', this.href, this.href);
+    });
+
+    $(".header").click(function(e) {  if (e.shiftKey) $(".footerFrame, .contentFrame, .headerFrame").toggleClass("debug"); });
+
+
+});
+
+
+/**
+ * Aciona um registro no Google Analytics
+ * Registra e identifica um clique no Google Analytics via evento de JavaScript
+ *
+ */
+function trackEvent(category, action, redirectTo)
+{
+    try {
+      _gaq.push(['_trackEvent', category, action]);
+      // alert( 'tracked event: ' + category + ', ' + action)
+    }catch(err){}
+}
